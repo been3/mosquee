d182b2035f10948ad42af436c0e41c5a568c8f51
esuau <evan.suau@etu.u-pec.fr>
2016-02-13 00:39:04 +0100

    Added basic views
    header and footer.

---
+++ view/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div id="social_icons">
+        <a href="http://twitter.com" target="_blank" id="social_icons__twitter"></a>
+        <a href="http://facebook.com" target="_blank" id="social_icons__facebook"></a>
+        <a href="http://instagram.com" target="_blank" id="social_icons__instagram"></a>
+        <a href="http://plus.google.com" target="_blank" id="social_icons__gplus"></a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Plan du site</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </nav>
+    <div id="credits_copyright">
+        <p>
+            Copyright &copy; 2016 Alex Benoit &amp; Evan Suau<br />
+            All rights reserved.
+        </p>
+    </div>
+</footer>
---
+++ view/header.php
@@ -0,0 +1,35 @@
+<header>
+    <div>
+        <a href="http://les-ziags.dev/test/">
+            <img src="img/logos/leaf32.png" alt="Les Ziags logo" />
+            <h3>Hôtel</h3>
+            <h2 id="logo_title_rotate">Les</h2>
+            <h1>Ziags</h1>
+            <div></div>
+        </a>
+    </div>
+    <nav id="main_menu">
+        <ul>
+            <li><a href="#">L'Hôtel</a></li>
+            <li>
+                <a href="#">Nos chambres</a>
+                <ul>
+                    <li><a href="#" class="main_menu__ourrooms__link">Chambre simple</a></li>
+                    <li><a href="#" class="main_menu__ourrooms__link">Chambre double</a></li>
+                    <li><a href="#" class="main_menu__ourrooms__link">Suite familiale</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Réservations</a></li>
+            <li>
+                <a href="#">Qui sommes nous ?</a>
+                <ul>
+                    <li><a href="#" class="main_menu__who__link">Le mot du directeur</a></li>
+                    <li><a href="#" class="main_menu__who__link">L'équipe</a></li>
+                </ul>
+            </li>
+            </li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#" id="main_menu__login__link"></a></li>
+        </ul>
+    </nav>
+</header>
