bcc0c060236628424c3c68dbd9551c9879f4cde2
esuau <evan.suau@etu.u-pec.fr>
2016-02-13 00:46:08 +0100

    Added script.js

---
+++ content/js/script.js
@@ -0,0 +1,13 @@
+(function($) {
+    var nav = $("#main_menu");
+    nav.find("li").each(function() {
+        if ($(this).find("ul").length > 0) {
+            $(this).mouseenter(function() {
+                $(this).find("ul").stop(true, true).slideDown();
+            });
+            $(this).mouseleave(function() {
+                $(this).find("ul").stop(true, true).slideUp();
+            });
+        }
+    });
+})(jQuery);
