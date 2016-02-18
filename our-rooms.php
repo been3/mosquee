7299a8123aaaff061d32c8819f5520e792b3b7d6
esuau <evan.suau@etu.u-pec.fr>
2016-02-18 22:56:21 +0100

    Added page "Nos chambres"

---
+++ our-rooms.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+    ╭──────────────────────────────────────────╮
+    │ ≡ Design & Front-end                     │
+    ╞══════════════════════════════════════════╡
+    │                                          │
+    │ EVAN SUAU                                │
+    ├──────────┬───────────────────────────────┤
+    │ Email    │ evan.suau@etu.u-pec.fr        │
+    ├──────────┼───────────────────────────────┤
+    │ Location │ Sucy-en-Brie, FR              │
+    ╰──────────┴───────────────────────────────╯
+
+    ╭──────────────────────────────────────────╮
+    │ ≡ Front-end & Back-end                   │
+    ╞══════════════════════════════════════════╡
+    │                                          │
+    │ ALEX BENOIT                              │
+    ├──────────┬───────────────────────────────┤
+    │ Email    │ benalex75@gmail.com           │
+    ├──────────┼───────────────────────────────┤
+    │ Location │ Paris, FR                     │
+    ╰──────────┴───────────────────────────────╯
+-->
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hôtel Les Ziags | Nos chambres</title>
+        <!--[if lt IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link href="content/css/main.css" rel="stylesheet" type="text/css" media="screen" />
+        <link href="content/css/home.css" rel="stylesheet" type="text/css" media="screen" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <!--[if IE]>
+            <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <![endif]-->
+
+        <script type="text/javascript" src="content/js/jquery.js"></script>
+        <script type="text/javascript" src="content/js/modernizr.js"></script>
+    </head>
+    <body>
+        <?php include 'view/headerView.php'; ?>
+        <?php include 'view/footerView.php'; ?>
+        <script type="text/javascript" src="content/js/script.js"></script>
+    </body>
+</html>
