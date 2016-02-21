c6d94f5f21dad9acf2acc841d8f7ba5583be2b6a
esuau <evan.suau@etu.u-pec.fr>
2016-02-22 00:55:51 +0100

    Redesign sign-in and sign-up forms

--- login.php
+++ login.php
@@ -37,35 +37,37 @@
             <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
         <![endif]-->
 
-        <script type="text/javascript" src="js/jquery.js"></script>
-        <script type="text/javascript" src="js/modernizr.js"></script>
+        <script type="text/javascript" src="/content/js/jquery.js"></script>
+        <script type="text/javascript" src="/content/js/modernizr.js"></script>
     </head>
     <body>
         <?php include 'view/headerView.php'; ?>
-        <?php include 'view/footerView.php'; ?>
 
         <section>
-            <div class="form-container" id="form-signin">
-                <form action="/authorize" method="post">
+            <div class="container">
+                <form action="/authorize" method="post" id="form-signin">
                     <h5>Se connecter</h5>
-                    <div><span><img src="content/img/icons/mail-envelope_32px_485160.png" alt="username" /></span><input type="text" placeholder="Email" name="username" /></div>
-                    <div><span><img src="content/img/icons/closed-padlock_16px_485160.png" alt="password" /></span><input type="password" placeholder="Mot de passe" name="password" /></div>
-                    <div><a href="forgot-password">Mot de passe oublié ?</a></div>
-                    <div><button>Se connecter</button></div>
+                    <span><img src="content/img/icons/mail-envelope_32px_485160.png" alt="username" /></span>
+                    <input type="text" placeholder="Email" name="username" />
+                    <span><img src="content/img/icons/closed-padlock_16px_485160.png" alt="password" /></span>
+                    <input type="password" placeholder="Mot de passe" name="password" />
+                    <a href="forgot-password">Mot de passe oublié ?</a>
+                    <button>Se connecter</button>
                 </form>
             </div>
-            <div class="form-container" id="form-signup">
-                <form action="signup-reques" method="post">
+            <div class="container">
+                <form action="signup-reques" method="post" id="form-signup">
                     <h5>Pas encore inscrit ?</h5>
                     <h3>Créer un compte</h3>
                     <input type="text" placeholder="Nom" name="firstname" />
                     <input type="text" placeholder="Prénom" name="lastname" />
                     <input type="text" pattern="[a-zA-Z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?" placeholder="Email" name="email" oninput="try {if ($(this).val().match(new RegExp(this.getAttribute('pattern')))) { this.setCustomValidity(''); } else { this.setCustomValidity('Veuillez entrer une adresse valide'); }} catch(e) {}"/><br />
-                    <button class="btn btn-primary btn-lg eml-submit-btn">S'inscrire</button>
+                    <button>S'inscrire</button>
                 </form>
             </div>
         </section>
 
+        <?php include 'view/footerView.php'; ?>
         <script type="text/javascript" src="js/script.js"></script>
     </body>
 </html>
