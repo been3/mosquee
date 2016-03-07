70591af11b1b2ff3b1fcf13284a369e3f513666a
esuau <evan.suau@etu.u-pec.fr>
2016-03-07 23:03:50 +0100

    Added sitemap page.

---
+++ sitemap.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--
+    ╭──────────────────────────────────────────╮
+    │ ≡ Design & Front-end                     │
+    ╞══════════════════════════════════════════╡
+    │                                          │
+    │ EVAN SUAU                                │
+    ├──────────┬───────────────────────────────┤
+    │ Email    │ evan.suau@etu.u-pec.fr        │
+    ├──────────┼───────────────────────────────┤
+    │ Location │ Sucy-en-Brie, FR              │
+    ╰──────────┴───────────────────────────────╯
+
+    ╭──────────────────────────────────────────╮
+    │ ≡ Front-end & Back-end                   │
+    ╞══════════════════════════════════════════╡
+    │                                          │
+    │ ALEX BENOIT                              │
+    ├──────────┬───────────────────────────────┤
+    │ Email    │ ben75.alex@gmail.com          │
+    ├──────────┼───────────────────────────────┤
+    │ Location │ Paris, FR                     │
+    ╰──────────┴───────────────────────────────╯
+-->
+
+<html lang="fr">
+    <head>
+
+        <!-- META -->
+        <meta charset="utf-8" />
+        <title>Hôtel Les Ziags | Contactez-nous</title>
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- STYLES -->
+        <!-- CSS -->
+        <link href="/content/css/main.css" rel="stylesheet" type="text/css" media="screen" />
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <!--[if IE]>
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <![endif]-->
+
+        <script type="text/javascript" src="/content/js/jquery-1.12.1.min.js"></script>
+        <script type="text/javascript" src="/content/js/modernizr-3.3.1.min.js"></script>
+    </head>
+
+    <body>
+
+        <!-- Wrapper -->
+        <div class="wrapper">
+
+            <!-- Header -->
+            <?php include 'view/headerView.php'; ?>
+
+            <!-- Content -->
+            <div class="content sitemap">
+                <div class="content-overlay">
+
+                    <!-- Container -->
+                    <div class="container container-main">
+                        <h1>Plan du site</h1>
+
+                        <ul>
+                            <li><a href="/index">Accueil</a></li>
+                            <li><a href="/hotel">L'hôtel</a></li>
+                            <li>
+                                <a href="/bedrooms">Nos chambres</a>
+                                <ul>
+                                    <li><a href="/bedrooms#single-room">Chambre simple</a></li>
+                                    <li><a href="/bedrooms#double-room">Chambre double</a></li>
+                                    <li><a href="/bedrooms#twin-room">Chambre twin</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="/booking/stay">Réservations</a></li>
+                            <li><a href="/about">À propos</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                            <li><a href="/login">Connexion</a></li>
+                        </ul>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Footer -->
+            <?php include 'view/footerView.php'; ?>
+        </div>
+
+        <script type="text/javascript" src="/content/js/script.js"></script>
+        <script type="text/javascript" src="/content/js/datePicker.js"></script>
+
+    </body>
+</html>
