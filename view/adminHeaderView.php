99450ccb8e948ed8ad84484438ef23a45c6b82f8
esuau <evan.suau@etu.u-pec.fr>
2016-03-15 10:45:08 +0100

    Extracted common views in admin interface

---
+++ view/adminHeaderView.php
@@ -0,0 +1,9 @@
+<!-- Header in admin pages -->
+<header>
+    <nav>
+        <ul>
+            <li><a href="/"><img src="/favicon.ico" /> LES ZIAGS</a></li>
+            <li><a href="">ADMINISTRATEUR <img src="/content/img/icons/avatar-admin_16px_white.png" /></a></li>
+        </ul>
+    </nav>
+</header>
