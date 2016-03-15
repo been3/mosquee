852b908360ce72804810fb109c4523fcb7ce9832
esuau <evan.suau@etu.u-pec.fr>
2016-03-15 10:47:25 +0100

    Rearrange admin interface structure.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -41,44 +41,17 @@
         <div class="wrapper">
 
             <!-- Header -->
-            <header>
-                <nav>
-                    <ul>
-                        <li><a href="/"><img src="/favicon.ico" /> LES ZIAGS</a></li>
-                        <li><a href="">ADMINISTRATEUR <img src="/content/img/icons/avatar-admin_16px_white.png" /></a></li>
-                    </ul>
-                </nav>
-            </header>
+            <?php include '../view/adminHeaderView.php' ?>
+
+            <!-- Aside -->
+            <?php include '../view/adminAsideView.php' ?>
 
             <!-- Content -->
             <div class="content admin dashboard">
 
-                <!-- Aside -->
-                <aside>
-                    <nav class="aside-nav">
-                        <ul>
-                            <li><a href="/admin/dashboard">Tableau de bord</a></li>
-                            <li><a href="/admin/stats">Stats</a></li>
-                            <li>
-                                <a href="">Pages</a>
-                                <ul>
-                                    <li><a href="">Modifier</a></li>
-                                    <li><a href="">Nouvelle</a></li>
-                                    <li><a href="">Supprimer</a></li>
-                                </ul>
-                            </li>
-                            <li><a href=""></a></li>
-                        </ul>
-                    </nav>
-                </aside>
-
-                <div class="container-main">
-                    <img src="/content/img/illustrations/analytics.jpg">
-                </div>
-
                 <!-- Container -->
                 <div class="container container-main">
-
+                    <img src="/content/img/illustrations/analytics.jpg">
                 </div>
 
             </div>
