6c408115fe1f38958c93cc5905a44a5d632acc86
esuau <evan.suau@etu.u-pec.fr>
2016-04-07 15:55:31 +0200

    Added prcing page and editing interface

--- admin/bedrooms.php
+++ admin/bedrooms.php
@@ -17,7 +17,7 @@
 
         <!-- META -->
         <meta charset="utf-8" />
-        <title>Hôtel Les Ziags | Tableau de bord</title>
+        <title>Dashboard | Chambres</title>
         <!--[if lt IE 9]>
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
