1971b24b0d4aa35a3828e7a865ff9426a3485134
esuau <evan.suau@etu.u-pec.fr>
2016-04-11 10:10:54 +0200

    Added generic SQL requests

--- data/requetes.sql
+++ data/requetes.sql
@@ -21,3 +21,63 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
+
+--
+-- 1) Quels sont les clients ayant annulé leurs réservations sur le premier trimestre 2016 ?
+--
+
+Select c.`client_lastname` AS nom, c.`client_firstname` AS prenom
+FROM `booking` b, `client` c
+WHERE b.`booking_idclient` = c.`client_idclient`
+  AND b.`booking_canceled` = 1
+  AND (b.`booking_datestart` BETWEEN '2016-01-01' AND'2016-03-31');
+
+--
+-- 2) Quel est le client ayant réservé le plus de nuitées en 2015 ?
+--
+
+Select c.`client_lastname` AS nom, c.`client_firstname` AS prenom
+FROM `booking` b, `client` c
+WHERE b.`booking_idclient` = c.`client_idclient`
+  AND(b.`booking_datestart` BETWEEN '2015-01-01' AND '2015-12-31'
+  OR b.`booking_dateend` BETWEEN '2015-01-01' AND '2015-12-31')
+Group by b.`booking_nbnights`
+ORDER BY SUM(b.`booking_nbnights`) DESC
+LIMIT 1;
+
+--
+-- 3) Quels sont les clients ayant réservé plus de 3 nuits (en une ou plusieurs fois) en 2015 ?
+--
+
+Select c.`client_lastname` AS nom, c.`client_firstname` as prenom
+FROM `booking` b, `client` c
+WHERE b.`booking_idclient` = c.`client_idclient`
+  AND(b.`booking_datestart` BETWEEN '2015-01-01' AND '2015-12-31'
+  OR b.`booking_dateend` BETWEEN '2015-01-01' AND '2015-12-31')
+Group by b.`booking_nbnights` HAVING SUM(b.`booking_nbnights`) > 3;
+
+--
+-- 4) Quelle est la capacité totale de lhôtel à ce jour ?
+--
+
+SELECT SUM(r.`room_capacity`) AS capacité
+FROM `room` r;
+
+--
+-- 5) Quel est le chiffre daffaire réalisé en janvier 2016 ?
+--
+
+SELECT SUM(b.`booking_price`) AS CAdeJanvier
+FROM `booking` b
+WHERE (b.`booking_datestart` BETWEEN '2016-01-01' AND '2016-01-31');
+
+--
+-- 6) Quel est le taux doccupation moyen du mois de janvier 2016 ?
+--
+
+SELECT SUM(b.`booking_nbnights`)/(12*31) AS txOccupation
+FROM `booking` b, `client` c
+WHERE b.`booking_idclient` = c.`client_idclient`
+  AND (b.`booking_datestart` BETWEEN '2016-01-01' AND '2016-01-31');
+
+
