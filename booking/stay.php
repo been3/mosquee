c3e83acbab60d29777d31c3d2891b219480b5b7a
esuau <evan.suau@etu.u-pec.fr>
2016-05-17 23:53:07 +0200

    Apply MVC architecture on booking # Create bookingModel # Rationalize PHP functions # Code cleanup & rearrange

--- booking/stay.php
+++ booking/stay.php
@@ -1,167 +1,36 @@
-<!DOCTYPE html>
-
-<!--
-    ╭──────────────────────────────────────────╮
-    │ ≡ Design & Front-end                     │
-    ╞══════════════════════════════════════════╡
-    │                                          │
-    │ EVAN SUAU                                │
-    ├──────────┬───────────────────────────────┤
-    │ Email    │ evan.suau@etu.u-pec.fr        │
-    ├──────────┼───────────────────────────────┤
-    │ Location │ Sucy-en-Brie, FR              │
-    ╰──────────┴───────────────────────────────╯
-
-    ╭──────────────────────────────────────────╮
-    │ ≡ Back-end                               │
-    ╞══════════════════════════════════════════╡
-    │                                          │
-    │ ALEX BENOIT                              │
-    ├──────────┬───────────────────────────────┤
-    │ Email    │ ben75.alex@gmail.com          │
-    ├──────────┼───────────────────────────────┤
-    │ Location │ Paris, FR                     │
-    ╰──────────┴───────────────────────────────╯
--->
-
-<html lang="fr">
-    <head>
-
-        <!-- META -->
-        <meta charset="utf-8" />
-        <title>Hôtel Les Ziags | Réservation | Choix du séjour</title>
-        <!--[if lt IE 9]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-
-        <!-- STYLES -->
-        <!-- CSS -->
-        <link href="/content/css/main.css" rel="stylesheet" type="text/css" media="screen" />
-        <link href="/content/css/booking.css" rel="stylesheet" type="text/css" media="screen" />
-        <link href="/content/js/jquery-ui-1.11.4/jquery-ui.min.css" rel="stylesheet" type="text/css" media="screen" />
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <!--[if IE]>
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <![endif]-->
-
-        <script type="text/javascript" src="/content/js/jquery-1.12.1.min.js"></script>
-        <script type="text/javascript" src="/content/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-        <script type="text/javascript" src="/content/js/modernizr-3.3.1.min.js"></script>
-    </head>
-
-    <body>
-
-        <!-- Wrapper -->
-        <div class="wrapper">
-
-            <!-- Header -->
-            <?php include '../view/headerView.php'; ?>
-
-            <!-- Content -->
-            <div class="content booking booking-stay">
-                <div class="content-overlay">
-
-                    <!-- Steps progression -->
-                    <?php include '../view/bookingProgressionView.php' ?>
-
-                    <!-- Form -->
-                    <div class="container container-main">
-                        <form action="/booking/pay" method="post">
-                            <table>
-                                <tr>
-                                    <td>
-                                        <label for="arrival-datepicker">Date d'arrivée</label>
-                                        <div class="input-container input-container-date"><input type="text" placeholder="dd/mm/aaaa" id="arrival-datepicker" class="input-date-arrival" name="arrivalDate"  required></div>
-                                        <label for="departure-datepicker">Date de départ</label>
-                                        <div class="input-container input-container-date"><input type="text" name="departureDate" placeholder="dd/mm/aaaa" id="departure-datepicker" class="input-date-departure" required></div>
-
-                                        <label for="booking-stay-bedroom-number">Nombre de personnes</label><br />
-                                        <input type="number" name='number' step="1" min="1" max="12" id="booking-stay-bedroom-number" required>
-                                    </td>
-                                    <td>
-                                        Formule
-                                        <div class="input-switch">
-                                            <input type="checkbox" class="input-switch-checkbox" id="board-switch" name="board" value = "1" checked>
-                                            <label class="input-switch-label" for="board-switch" id="board-switch-label">
-                                                <span class="input-switch-inner"></span>
-                                                <span class="input-switch-switch"></span>
-                                            </label>
-                                        </div>
-                                        <br />
-
-                                        <div class="input-switch">
-                                            <input type="checkbox" class="input-switch-checkbox" id="bathroom-switch" name="bathroom" value ="1" checked>
-                                            <label class="input-switch-label" for="bathroom-switch" id="bathroom-switch-label">
-                                                <span class="input-switch-inner"></span>
-                                                <span class="input-switch-switch"></span>
-                                            </label>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p>
-                                            Motif<br />
-                                            <input type="radio" name="purpose" value="business" id="booking-stay-purpose-business" required /><label for="booking-stay-purpose-business">  Voyage d'affaire</label><br />
-                                            <input type="radio" name="purpose" value="tourism" id="booking-stay-purpose-tourism" /><label for="booking-stay-purpose-tourism">  Tourisme</label><br />
-                                            <input type="radio" name="purpose" value="none" id="booking-stay-purpose-none" /><label for="booking-stay-purpose-none">  Autre</label>
-                                        </p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3">
-                                        <button class="button-submit">Réserver ></button>
-                                    </td>
-                                </tr>
-                            </table>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
 <?php
-include '../content/phpFunctions/generateRandId.php';
-include '../content/phpFunctions/calculatePrice.php';
-include '../content/phpFunctions/calculateNbNights.php';
-include '../content/phpFunctions/checkRoomAvailable.php';
 
+require_once '../model/bookingModel.php';
+require_once '../view/bookingStayView.php';
 
-            if (isset($_POST['bathroom'])){$bathroom = 1;}else{$bathroom=0;}
-            if (isset($_POST['board'])){$board = 1;}else{$board=0;}
-            $arrivalDate =(isset($_POST['arrivalDate']) ? $_POST['arrivalDate'] : '');
-            //$arrivalDate = date_format($arrivalDate,'d-m-Y:i:s');
-            $departureDate =(isset($_POST['departureDate']) ? $_POST['departureDate'] : '');
-            $idBooking = generateRandId();
-            $loginUser = $_SESSION["login_user"];
-            $idClient = "SELECT client_idclient FROM client where client_clientlogin = $loginUser";
-            $idPayment = "SELECT `payment_idpayment` FROM `payment` WHERE `payment_idclient` = $idClient";
-            $nbPersons = (isset($_POST['number']) ? $_POST['number'] : "" );
-            $idRoom = checkRoomAvailable($bathroom ,$nbPersons,$arrivalDate,$departureDate);
-            $nbNights = calculateNbNights($arrivalDate,$departureDate);
-            $price = calculatePrice($nbNights,$arrivalDate,$idRoom,$board);
-            $canceled = 0;
-            if (isset($_POST['number'])){$nbPersons = $_POST['number'];}
-            $purpose = (isset($_POST['purpose']) ? $_POST['purpose'] : '');
-            $newBooking = "INSERT INTO booking (booking_idbooking, booking_idroom, booking_idclient,booking_idpayment,booking_datestart, booking_dateend, booking_nbnights, booking_price, booking_canceled,booking_nbpersons,booking_purpose)
-            VALUES ('$idBooking', $idRoom,$idClient,$idPayment,$arrivalDate,$departureDate,$nbNights,$price,$canceled,$nbPersons,$purpose";
-            ?>
+$loginUser = $_SESSION["login_user"];
 
+if (isset($_POST['bathroom'])) {
+    $bathroom = 1;
+} else {
+    $bathroom = 0;
+}
 
-            <!-- Footer -->
-            <?php include '../view/footerView.php'; ?>
-        </div>
+if (isset($_POST['board'])){
+    $board = 1;
+} else {
+    $board=0;
+}
 
+$arrivalDate =(isset($_POST['arrivalDate']) ? $_POST['arrivalDate'] : '');
 
-        <script type="text/javascript" src="/content/js/script.js"></script>
-        <script type="text/javascript" src="/content/js/datePicker.js"></script>
-        <script>
-            $("#booking-stay-bedroom-number").spinner({
-                min : 1,
-                max : 12,
-                showOn : 'both'
-            });
-        </script>
+//$arrivalDate = date_format($arrivalDate,'d-m-Y:i:s');
 
-    </body>
-</html>
+$departureDate = (isset($_POST['departureDate']) ? $_POST['departureDate'] : '');
+$idBooking = generateRandId();
+$idClient = "SELECT `client_idclient` FROM `client` WHERE `client_clientlogin` = '$loginUser'";
+$idPayment = "SELECT `payment_idpayment` FROM `payment` WHERE `payment_idclient` = '$idClient'";
+$nbPersons = (isset($_POST['number']) ? $_POST['number'] : "" );
+$idRoom = checkRoomAvailable($bathroom, $nbPersons, $arrivalDate, $departureDate);
+$nbNights = calculateNbNights($arrivalDate, $departureDate);
+$price = calculatePrice($nbNights, $arrivalDate, $idRoom,$board);
+$canceled = 0;
+if (isset($_POST['number'])){$nbPersons = $_POST['number'];}
+$purpose = (isset($_POST['purpose']) ? $_POST['purpose'] : '');
 
-
+$newBooking = "INSERT INTO `booking` (`booking_idbooking`, `booking_idroom`, `booking_idclient`, `booking_idpayment`, `booking_datestart`, `booking_dateend`, `booking_nbnights`, `booking_price`, `booking_canceled`, `booking_nbpersons`, `booking_purpose`) VALUES ('$idBooking', '$idRoom', '$idClient', '$idPayment', '$arrivalDate', '$departureDate', '$nbNights', '$price', '$canceled', '$nbPersons', '$purpose'";
--- model/bookingModel.php
+++ model/bookingModel.php
@@ -1,12 +1,61 @@
 <?php
-function checkRoomAvailable($bathroom,$numberPersons,$dateStart,$dateEnd){ //returns booking_roomnbr if a room is free else returns 0
-    if ($bathroom == 1 && $numberPersons == 1){ //rooms : nbr1 nbr3
-       $lastDateStart = "SELECT b.booking_datestart FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '1' AND b.booking_datestart < $dateEnd  AND b.booking_canceled = '0' ORDER BY b.booking_datestart DESC LIMIT 1";
-       $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '1' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
-       if ($lastDateStart <$lastDateEnd && $lastDateEnd < $dateStart){return 1;}
-       $lastDateStart = "SELECT b.booking_datestart FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '3' AND b.booking_datestart < $dateEnd AND b.booking_canceled = '0' ORDER BY b.booking_datestart DESC LIMIT 1";
-       $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '3' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
-       if ($lastDateStart <$lastDateEnd && $lastDateEnd < $dateStart){return 3;}else{return 0;}
+
+define('DB_SERVER', 'localhost:3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lesziagsdb');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+session_start();
+
+function generateRandId($length = 10) {
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+function calculatePrice($nbNights, $dateStart, $idRoom, $board) {
+    $dateMonth = date("m",strtotime($dateStart));
+    if ($dateMonth>4 or $dateMonth<11) {
+        $price = intval("SELECT `room_highpricing` FROM `room` WHERE `room_idroom` = '$idRoom'");
+        if ($board == 1) {
+            $price += 15;
+        }
+        $price *= $nbNights;
+        return $price;
+
+    } else {
+        $price = intval("SELECT `room_lowpricing` FROM `room` WHERE `room_idroom` = '$idRoom'");
+        if ($board == 1) {
+            $price += 15;
+        }
+        $price *= $nbNights;
+        return $price;
+    }
+}
+
+function calculateNbNights($start, $end) {
+    $ts1 = strtotime($start);
+    $ts2 = strtotime($end);
+    $seconds_diff = $ts2 - $ts1;
+    $day_diff = $seconds_diff/(60*60*24);
+    return $day_diff;
+}
+
+function checkRoomAvailable($bathroom,$numberPersons,$dateStart,$dateEnd) {
+    //returns booking_roomnbr if a room is free else returns 0
+    if ($bathroom == 1 && $numberPersons == 1){
+        //rooms : nbr1 nbr3
+        $lastDateStart = "SELECT b.booking_datestart FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '1' AND b.booking_datestart < $dateEnd  AND b.booking_canceled = '0' ORDER BY b.booking_datestart DESC LIMIT 1";
+        $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '1' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
+        if ($lastDateStart <$lastDateEnd && $lastDateEnd < $dateStart){return 1;}
+        $lastDateStart = "SELECT b.booking_datestart FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '3' AND b.booking_datestart < $dateEnd AND b.booking_canceled = '0' ORDER BY b.booking_datestart DESC LIMIT 1";
+        $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '3' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
+        if ($lastDateStart <$lastDateEnd && $lastDateEnd < $dateStart){return 3;}else{return 0;}
     }
     if ($bathroom == 0 && $numberPersons == 1){ //rooms : nbr2 nbr4
         $lastDateStart = "SELECT b.booking_datestart FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '2' AND b.booking_datestart < $dateEnd AND b.booking_canceled = '0' ORDER BY b.booking_datestart DESC LIMIT 1";
@@ -27,7 +76,7 @@ function checkRoomAvailable($bathroom,$numberPersons,$dateStart,$dateEnd){ //ret
         $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '9' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
         if ($lastDateStart <$lastDateEnd && $lastDateEnd < $dateStart){return 9;}
         $lastDateStart = "SELECT b.booking_datestart FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '11' AND b.booking_datestart < $dateEnd AND b.booking_canceled = '0' ORDER BY b.booking_datestart DESC LIMIT 1";
-         $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '11' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
+        $lastDateEnd = "SELECT b.booking_dateend FROM booking b , room r where b.booking_idroom = r.room_idroom AND r.room_roomnbr = '11' AND b.booking_dateend< $dateEnd ORDER BY b.booking_datestart DESC LIMIT 1";
         if ($lastDateStart <$lastDateEnd && $lastDateEnd < $dateStart){return 11;}else{return 0;}
     }
     if ($bathroom == 0 && $numberPersons == 2) { //rooms : nbr6 nbr8 nbr10 nbr12
@@ -53,6 +102,9 @@ function checkRoomAvailable($bathroom,$numberPersons,$dateStart,$dateEnd){ //ret
         } else {
             return 0;
         }
-    }else{return 0;}
+    } else {
+        return 0;
+    }
 
 }
+
